2feb2f42831ef48f194896dcb2a2a8d365a96509
HariyaNobuki <hariya-nobuki-vr@ynu.jp>
2024-02-09 15:03:33 +0900

    OK

--- forstu.py
+++ forstu.py
@@ -13,11 +13,11 @@ from PyPDF4 import PdfFileWriter
 """ Member list """
 
 M2 = ["来栖魁人","針谷亘輝",
-      #"平岡巧光","三浦岳也",
-      #"洞口裕真","川﨑弘貴",
-      #"須藤駿","小山天輔",
-      #"坪井陽人","藤澤大世",
-      #"渡慶次一哲"
+      "平岡巧光","三浦岳也",
+      "洞口裕真","川﨑弘貴",
+      "須藤駿","小山天輔",
+      "坪井陽人","藤澤大世",
+      "渡慶次一哲"
       ]
 
 
@@ -37,13 +37,13 @@ if __name__ == '__main__':
     """横"""
     M2_path = os.path.join(pdf,"2in1予稿")
     merger = pypdf.PdfMerger()
-    merger.append(os.path.join(pdf,"表紙","2023","表紙2023.pdf"))
+    merger.append(os.path.join(pdf,"表紙","2023","表紙2023_2in1.pdf"))
     for ind in M2:
         print(ind)
         #ind_path = os.path.join(M2_path,ind)
         merger.append(os.path.join(M2_path,"2023_予稿_%s.pdf"%(ind)))
         #merger.append(os.path.join(ind_path,os.path.join(ind_path,"%s_中間発表_スライド_4in1-横.pdf"%(ind))))
-    merger.write(os.path.join(pdf,'2023年度大学院輪講_中田研_M2_横.pdf'))
+    merger.write(os.path.join(pdf,'2023年度中田研_予稿集.pdf'))
     merger.close()
 
 
@@ -55,8 +55,8 @@ if __name__ == '__main__':
     #pdf_reader = PdfFileReader(open(pdf_file_path, 'rb'), strict=True)
 
     ## Output
-    output = PdfFileWriter()
-    output_name = os.path.join(pdf,'2023年度大学院輪講_中田研_M2_横_1733.pdf')
-    output_stream = open(output_name, 'wb')
-    output.write(output_stream)
-    output_stream.close()
+    #output = PdfFileWriter()
+    #output_name = os.path.join(pdf,'2023年度大学院輪講_中田研_M2_横_1733.pdf')
+    #output_stream = open(output_name, 'wb')
+    #output.write(output_stream)
+    #output_stream.close()
